25bad0684798cd3ded731dfa33df3c14f50248f9
dmitin-bv <denys.mitin@bazaarvoice.com>
2016-06-18 15:57:39 +0300

    completed v3

--- v3/main.js
+++ v3/main.js
@@ -1,6 +1,40 @@
 
 var displayInput = document.getElementById('display');
+var previous;
+var operationName;
 
 function typeNumber(number) {
     displayInput.value += number;
+}
+
+function reset() {
+    displayInput.value = '';
+}
+
+function operation(name) {
+    previous = parseFloat(displayInput.value);
+    reset();
+    operationName = name;
+}
+
+function calculate() {
+    var current = parseFloat(displayInput.value)
+    var result;
+    switch (operationName) {
+        case 'add':
+            result = previous + current;
+            break;
+        case 'substract':
+            result = previous - current;
+            break;
+        case 'multiply':
+            result = previous * current;
+            break;
+        case 'divide':
+            result = previous / current;
+            break;
+        default:
+            result = 'unsupported operation';
+    }
+    displayInput.value = result;
 }
